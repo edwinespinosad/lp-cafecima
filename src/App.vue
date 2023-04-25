48a59a40e6da04b95dd88a8cadd2e71ae210f0ff
edwin <edwin.satoritech@gmail.com>
2023-04-25 14:19:17 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+
+<Navbar></Navbar>
+<Slider></Slider>
+<OurProducts></OurProducts>
+
+</template>
+
+<style scoped>
+</style>
