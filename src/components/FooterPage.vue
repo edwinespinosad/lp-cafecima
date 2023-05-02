22ef178ef68ec70d88c74042727f2196cffb7088
edwin <edwin.satoritech@gmail.com>
2023-05-02 14:54:28 -0600

    footer correction

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,19 +1,9 @@
 <template >
-  <footer
-    class="text-white text-center py-5 my-5"
-  >
-    {{ new Date().getFullYear() }} <span class="m-1">&#169;</span> CIMA
-    <Span class="position-relative m-1 top-3px">99</Span> Todos los derechos
-    reservados
-</footer>
+  <footer class="text-white text-center py-5 my-5">
+    {{ new Date().getFullYear() }} &#169; CIMA 99 Todos los derechos reservados
+  </footer>
 </template>
 <script>
 </script>
-<style scoped>
-.bgFooter {
-  background: #0c0b0b;
-}
-.top-3px{
-  top: 3px;
-}
+<style>
 </style>
